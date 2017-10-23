1f3c6364828aecf199df95a935150a8a8814b5fd
ganhui <1101485871@qq.com>
2017-10-23 23:22:58 +0800

    通用惰性单例

---
+++ 4chapter/signg.js
@@ -0,0 +1,19 @@
+var getSingle = function(fn) {
+    var result;
+    return function() {
+        return result || (result = fn.apply(this, arguments));
+    }
+}
+
+var person = function(name, id) {
+    this.name = name;
+    this.id = id;
+    console.log(name);
+    return this;
+}
+
+var proxyPerson = getSingle(person);
+
+var a = new proxyPerson("name1", 1);
+var b = new proxyPerson("name2", 2);
+console.log(a === b)
