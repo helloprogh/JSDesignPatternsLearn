ec9f8861b754d1a4155b4dc588ff4bb15da9e686
ganhui <1101485871@qq.com>
2017-10-24 22:12:58 +0800

    表单开头

--- 5chapter/celue.js
+++ 5chapter/celue.js
@@ -1,1 +1,17 @@
 //表单校验
+
+var validateRules = {
+    "Empty": function() {
+
+    },
+    "number": function() {
+
+    },
+    "regex": function() {
+
+    }
+}
+
+var validate = function() {
+    
+}
